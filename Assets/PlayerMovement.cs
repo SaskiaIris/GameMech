91ab4edef2c64b88f86f18ac6849871dcee75f5c
ikarian7 <scott.marchant7@gmail.com>
2019-10-02 19:14:43 +0200

    innitial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool playeractive = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && playeractive == true)
+        {
+            playeractive = false;
+        } else if(Input.GetKeyDown(KeyCode.X) && playeractive == false)
+        {
+            playeractive = true;
+        }
+
+        if (playeractive == true) {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
+        } 
+    }
+
+    private void FixedUpdate()
+    {
+        if (playeractive == true)
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coins"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    int speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float movement = horizontal * speed;
+        float jump = vertical * speed;
+        
+        transform.position = new Vector2(transform.position.x + movement, 0);
+        transform.position = new Vector2(0, transform.position.y + jump);
+    }
+}
